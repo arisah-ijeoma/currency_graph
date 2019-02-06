7a727c4720164fecc7665fbc05edb1e61e2ca67d
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-02-06 19:02:04 +0100

    accessing correct currency values

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class ChartsController < ApplicationController
+  def home
+    base = params[:cur] || 'BRL'
+    today = Date.today
+    @response ||= HTTParty.get('https://api.exchangeratesapi.io/history?'\
+    "start_at=#{today - 1.year}&end_at=#{today}&base=#{base}&symbols=USD,EUR,AUD")
+
+    # one base three other currencies
+    # base can be changed
+    # you can't compare base with itself OR you'll get 1
+    # symbols will have to be dynamic
+  end
 end
