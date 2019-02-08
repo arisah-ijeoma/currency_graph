2623696da4cd8c662c746b985780c1ff8aea2762
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-02-08 22:40:59 +0100

    refactor and optimize code

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -2,20 +2,25 @@
 
 class ChartsController < ApplicationController
   def home
-    base = params[:base] || 'BRL'
-    compare = params[:compare] || 'USD'
+    @base = params[:base] || 'BRL'
+    @compare = params[:compare] || 'USD'
     today = Date.today
 
-    # the API can not compare EUR
-    return nil if base == compare
+    symbols = (Currency::CURRENCIES - [@base]).join(',')
 
     begin
       @response ||= HTTParty.get('https://api.exchangeratesapi.io/history?'\
-      "start_at=#{today - 1.year}&end_at=#{today}&base=#{base}&symbols=#{compare}")
+      "start_at=#{today - 1.year}&end_at=#{today}&base=#{@base}&symbols=#{symbols}")
     rescue Errno::ECONNRESET => e
+      count = 0
       count += 1
       retry unless count > 5
-      puts "tried 10 times and couldn't get #{url}: #{e}"
+      puts "tried 5 times and couldn't get to api: #{e}"
+    end
+
+    respond_to do |f|
+      f.js { render layout: false, content_type: 'text/javascript' }
+      f.html
     end
   end
 end
