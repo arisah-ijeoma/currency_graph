80e123fb096501e37357621f819c6d36e0c4150c
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-02-08 23:58:59 +0100

    add more scenarios

--- spec/features/external_request_spec.rb
+++ spec/features/external_request_spec.rb
@@ -3,9 +3,19 @@
 require 'rails_helper'
 
 describe 'external request', type: :feature do
+  let(:api) { 'https://api.exchangeratesapi.io/latest?base=USD' }
+
   it 'queries exchangerate api' do
-    uri = URI('https://api.exchangeratesapi.io/latest?base=USD')
+    uri = URI(api)
     response = Net::HTTP.get(uri)
     expect(response).to be_an_instance_of(String)
+  end
+
+  it 'can timeout' do
+    stub_request(:any, api).to_timeout
+  end
+
+  it 'can raise errors' do
+    stub_request(:any, api).to_raise(Errno::ECONNRESET)
   end
 end
